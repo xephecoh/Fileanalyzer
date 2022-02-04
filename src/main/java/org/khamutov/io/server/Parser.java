180e85bce0cab5a15405616155a8d33c98d02841
xephecoh <xephecoh@gmail.com>
2022-02-04 12:46:38 +0200

    formatted

--- src/main/java/org/khamutov/io/server/Parser.java
+++ src/main/java/org/khamutov/io/server/Parser.java
@@ -1,14 +1,15 @@
 package org.khamutov.io.server;
 
-public  class Parser {
+public class Parser {
 
     public static String parseGet(String header) {
+
         String searchWord;
         searchWord = header.replaceAll("GET", "")
                 .replaceAll("HTTP/1.1", "")
                 .replaceAll("/", "");
-        if(searchWord.endsWith(".css")){
-            searchWord="/css"+searchWord;
+        if (searchWord.endsWith(".css")) {
+            searchWord = "/css" + searchWord;
         }
         return searchWord;
 
